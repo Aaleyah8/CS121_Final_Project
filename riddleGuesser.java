29347f06e942cbc7b216d0343350c1e3749445fd
Aaleyah8 <aaleyahveness8@gmail.com>
2025-04-23 01:49:03 +0000

    modified:   riddleGuesser.java

--- riddleGuesser.java
+++ riddleGuesser.java
@@ -3,9 +3,8 @@ import java.util.Random;
 
 public class riddleGuesser {
 	
-	List list = new ArrayList();
-
 	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
 		Scanner input = new Scanner(System.in);
 		boolean keepGoing = true;
 
@@ -19,32 +18,32 @@ public class riddleGuesser {
 			System.out.println("4) List Riddles");
 			System.out.println();
 			System.out.println("Your Choice: ");
-			String response = input.nextLine();
-			return Response;
+			String response = scanner.nextLine();
+			return response;
 		} // end while
 	} // end main
 
 	public static void loadRiddles(){
-		Scanner input = new Scanner(System.in);
+		
 		Riddle.add(new Riddle("I have a tail, and I have a head, but I have no body. What am I?", "A Coin"));
 		Riddle.add(new Riddle("What is always coming, but never arrives?", "A Future"));
 		Riddle.add(new Riddle("What is full of holes but still holds water?", "A Sponge"));
 	} // end constructor
 
 	public void playRiddle(){
-
+		Scanner scanner = new Scanner(System.in);
 		if (Riddle.isEmpty()){
 			System.out.println("No Riddles available");
 			return;
 		} // end if
 
-		int random = Math.random() * Riddles.size;
+		int random = Math.random() * Riddle.size;
 		Riddle currentRiddle = Riddle.get(random);
 
 		System.out.println("Heres a riddle: ");
 		System.out.println();
 		System.out.println("Your Answer: ");
-		String userAnswer = Scanner.nextLine();
+		String userAnswer = scanner.nextLine();
 
 		if (userAnswer.equals(currentRiddle.getAnswer())){
 			System.out.println("You Guessed right!");
@@ -52,54 +51,56 @@ public class riddleGuesser {
 			System.out.println("Sorry, not correct. The answer is: " + currentRiddle.getAnswer());
 		} // end else
 	} // end constructor
+		
 
 	public void addRiddle(){
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("-- Add a Riddle --");
 		System.out.println();
 		System.out.println("Enter your Riddle: ");
-		String question = Scanner.nextLine();
+		String question = scanner.nextLine();
 		System.out.println("Enter the Answer: ");
-		String answer = Scanner.nextLine();
+		String answer = scanner.nextLine();
 
 		Riddle newRiddle = new Riddle(question, answer);
-		Riddles.add(newRiddle);
+		Riddle.add(newRiddle);
 		System.out.println("Riddle Successfully added!");
 	} // end constructor
 
 	public void listRiddle(){
-		if (Riddles.isEmpty()){
+		if (Riddle.isEmpty()){
 			System.out.println("No riddles have been added yet");
 			return;
 		} // end if
 
 		System.out.println("-- Riddle List --");
 		for (int i = 0; i < Riddle.size(); i++) {
-			System.out.println((i + 1) + ". " + Riddles.get(i).getQuestion());
+			System.out.println((i + 1) + ". " + Riddle.get(i).getQuestion());
 		} // end for
 		System.out.println("---------------------");
 	} // end constructor
 
 	public void editRiddle(){
 	
-		if (Riddles.isEmpty()){
+		if (Riddle.isEmpty()){
 			System.out.println("No Riddles to edit. Please add some first.");
 			return;
 		} // end if
-
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("What Riddle do you want to edit: ");
-		String Edit = Scanner.nextLine();
-
-		if (Edit >  1 && Edit < Riddles.size()){
-			Riddle riddleEdit = Riddle.get(Edit - 1);
-			System.out.println("Edit Riddle" + Edit + " ---");
+		String edit = scanner.nextLine();
+		int edit; 
+		if (edit = 1 && edit <= Riddle.size()){
+			Riddle riddleEdit = Riddle.get(edit - 1);
+			System.out.println("Edit Riddle" + edit + " ---");
 			System.out.println("Current Question: " + riddleEdit.getQuestion());
 			System.out.println("Enter a new Question (or press ENTER to keep current): ");
-			String newQuestion = Scanner.nextLine();
+			String newQuestion = scanner.nextLine();
 			if (newQuestion.isEmpty()) {
 				riddleEdit.setQuestion(newQuestion);
 			} // end if
 
-			System.out.println("Riddle " + Edit + "Updated!");
+			System.out.println("Riddle " + edit + "Updated!");
 		} else {
 			System.out.println("Invalid");
 		} // end else
